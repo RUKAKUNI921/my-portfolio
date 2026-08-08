0267feb2c436f2f4e760765bd509adabc7da322c
ruka kunimoto <pasapasaobake@gmail.com>
2026-08-09 01:13:38 +0900

    about change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,10 +9,10 @@ export default function About() {
           <div className={`${styles.section}`}>
             <h2 className={`${styles.name} u-ff-txt`}>
               <span className={`${styles.ja} u-ff-ja-r u-trim`}>国本 琉海</span>
-              <span className={`${styles.en} u-trim`}>Ruka Kunimoto</span>
+              <span className={`${styles.en} u-ff-en-m u-trim`}>Ruka Kunimoto</span>
             </h2>
             <p className={`${styles.txt} u-ff-txt`}>
-              興味を持ったら、まず手を動かしてみる。新しい領域に踏み込むことをおそれず、まずアイデアを考え、実際に手を動かしながら形にしていくことを大切にしています。専門を一つに絞るのではなく、興味を持ったものにはできる限り取り組み、経験を積み重ねてきました。
+              興味を持ったら、まず手を動かしてみる。新しい領域に踏み込むことをおそれず、考え、実際に手を動かしながら形にしていくことを大切にしています。専門を一つに絞るのではなく、興味を持ったものにはできる限り取り組み、経験を積み重ねてきました。
               <br />
               <br />
               これからも、その時々で必要な技術や手法を自ら学びながら、自分にできることの幅を広げていきたいと考えています。
